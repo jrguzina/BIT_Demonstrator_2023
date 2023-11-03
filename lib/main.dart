9d7a84e6069e107765eeb137d943feeca26ff9f8
Luca Guzina <lu351guz@htwg-konstanz.de>
2023-11-03 23:41:12 +0100

    update, bosch model not working

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,7 @@ class StartPage extends StatelessWidget {
                   ),
                   padding: EdgeInsets.all(20),
                   child: ModelViewer(
+                    //src: 'assets/boschcamera/scene.gltf',
                     src: 'assets/Format2.glb',
                     autoPlay: true,
                     backgroundColor: Colors.transparent,
@@ -131,7 +132,6 @@ class StartPage extends StatelessWidget {
     );
   }
 }
-
 
 
 
@@ -206,63 +206,85 @@ class EnergyUsagePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Energieverwendung'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              'Energieverbrauch nach Raum',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20.0),
-            Container(
-              height: 300, // Adjust the height as needed
-              child: SfCartesianChart(
-                primaryXAxis: CategoryAxis(),
-                series: <ChartSeries>[
-                  ColumnSeries<ChartData, String>(
-                    dataSource: [
-                      ChartData('Küche', 30),
-                      ChartData('Wohnzimmer', 50),
-                      ChartData('Schlafzimmer', 40),
-                      ChartData('Badezimmer', 25),
-                      // Add more data points here
-                    ],
-                    xValueMapper: (ChartData data, _) => data.room,
-                    yValueMapper: (ChartData data, _) => data.value,
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Energieverbrauch nach Raum',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: 20.0),
-            Tile(
-              title: 'Aktueller Energieverbrauch',
-              content: 'Informationen über den aktuellen Energieverbrauch...',
-            ),
-            SizedBox(height: 20.0),
-            Tile(
-              title: 'Verlauf des Energieverbrauchs',
-              content:
-                  'Informationen über den Verlauf des Energieverbrauchs über die Zeit...',
-            ),
-            SizedBox(height: 20.0),
-            Tile(
-              title: 'Vergleiche mit Durchschnittsverbrauch',
-              content:
-                  'Vergleiche mit dem Durchschnittsverbrauch anderer Haushalte...',
-            ),
-            SizedBox(height: 20.0),
-            Tile(
-              title: 'Tipps zur Energieeinsparung',
-              content: 'Tipps zur effizienten Nutzung und Energieeinsparung...',
-            ),
-          ],
+              SizedBox(height: 20.0),
+              Container(
+                height: 300, // Höhe je nach Bedarf anpassen
+                child: SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  series: <ChartSeries>[
+                    ColumnSeries<ChartData, String>(
+                      dataSource: [
+                        ChartData('Küche', 30),
+                        ChartData('Wohnzimmer', 50),
+                        ChartData('Schlafzimmer', 40),
+                        ChartData('Badezimmer', 25),
+                        // Weitere Datenpunkte hier hinzufügen
+                      ],
+                      xValueMapper: (ChartData data, _) => data.room,
+                      yValueMapper: (ChartData data, _) => data.value,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Aktueller Energieverbrauch',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                height: 300,
+                child: SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  series: <ChartSeries>[
+                    LineSeries<ChartData, String>(
+                      dataSource: [
+                        ChartData('Küche', 40),
+                        ChartData('Wohnzimmer', 60),
+                        ChartData('Schlafzimmer', 50),
+                        ChartData('Badezimmer', 35),
+                        // Weitere Datenpunkte hier hinzufügen
+                      ],
+                      xValueMapper: (ChartData data, _) => data.room,
+                      yValueMapper: (ChartData data, _) => data.value,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Tile(
+                title: 'Verlauf des Energieverbrauchs',
+                content:
+                'Informationen über den Verlauf des Energieverbrauchs über die Zeit...',
+              ),
+              SizedBox(height: 20.0),
+              Tile(
+                title: 'Vergleiche mit Durchschnittsverbrauch',
+                content:
+                'Vergleiche mit dem Durchschnittsverbrauch anderer Haushalte...',
+              ),
+              SizedBox(height: 20.0),
+              Tile(
+                title: 'Tipps zur Energieeinsparung',
+                content: 'Tipps zur effizienten Nutzung und Energieeinsparung...',
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
 
 class ChartData {
   final String room;
