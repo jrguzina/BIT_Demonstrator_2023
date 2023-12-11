0890b010c3b66751cb8dfa2fd0974c80c6321edd
Niklas Mathauer <ni352mat@htwg-konstanz.de>
2023-12-11 10:01:03 +0100

    updated images

--- lib/co2_footprint_page.dart
+++ lib/co2_footprint_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -103,14 +105,32 @@ class MySwitchState extends State<MySwitch> {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      // Maximum width and height available for the image
+      double maxWidth = constraints.maxWidth;
+      double maxHeight = constraints.maxHeight;
+
+      // Calculate the scale factor
+      double widthScale = maxWidth / 1024;
+      double heightScale = maxHeight / 1024;
+      double scale = min(widthScale, heightScale);
+
+      // Scale is min of widthScale and heightScale, but not more than 1 (for scaleDown)
+      scale = scale > 1.0 ? 1.0 : scale;
+
+
+
+      // Use the scale as needed, for example, logging it or passing it to another widget
+      print('Scale factor: $scale');
     return
     Row(children:[
       Container(
-      width: 300,
-      height: 1250,
+      width: 290,
+      height: 1085,
       margin: EdgeInsets.all(25.0),
       child:
-      Stack(alignment: Alignment.topCenter, children: [
+      Stack(alignment: Alignment.center, children: [
           Image.asset('assets/phone_mockup.png', fit: BoxFit.contain),
 
       Column(
@@ -177,100 +197,6 @@ class MySwitchState extends State<MySwitch> {
               });
             },
           ),
-          SizedBox(height: 250),
-          Text(
-            'Energy Consumption per Day',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          LinearProgressIndicator(
-              value: (getResource("energyConsumption") /
-                  getFullResource(
-                      "energyConsumption")), // Assuming the price is a percentage of 100
-              backgroundColor: Colors.cyan
-                  .shade300, // Color of the background of the progress bar.
-              valueColor: AlwaysStoppedAnimation<Color>(Colors
-                  .cyan.shade900), // Color of the progress in the progress bar
-              minHeight: 20),
-          Text(
-            '${getResource("energyConsumption").round()} kwH', // Convert the progress to percentage and round it
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 40),
-          Text(
-            'Carbon Footprint over Product-Lifespan',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          LinearProgressIndicator(
-              value: (getResource("carbonFootprint") /
-                  getFullResource(
-                      "carbonFootprint")), // Assuming the price is a percentage of 100
-              backgroundColor: Colors.brown
-                  .shade200, // Color of the background of the progress bar.
-              valueColor: AlwaysStoppedAnimation<Color>(
-                  Colors.brown), // Color of the progress in the progress bar
-              minHeight: 20),
-          Text(
-            '${getResource("carbonFootprint").round()} tons of CO2', // Convert the progress to percentage and round it
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                      width: 60,
-                      height: 60,
-                      child: CircularProgressIndicator(
-                        value: getResource("security") /
-                            getFullResource("security"),
-                        backgroundColor: Colors.lightBlueAccent.shade100,
-                        color: Colors.blue,
-                        strokeWidth: 10,
-                      )),
-                  SizedBox(height: 20),
-                  Text('Security'),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                      width: 60,
-                      height: 60,
-                      child: CircularProgressIndicator(
-                        value:
-                            getResource("comfort") / getFullResource("comfort"),
-                        backgroundColor: Colors.lightBlueAccent.shade100,
-                        color: Colors.blue,
-                        strokeWidth: 10,
-                      )),
-                  SizedBox(height: 20),
-                  Text('Comfort'),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 40),
-          Text(
-            'Price',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          LinearProgressIndicator(
-              value: (getResource("price") /
-                  getFullResource(
-                      "price")), // Assuming the price is a percentage of 100
-              backgroundColor: Colors.lightGreen
-                  .shade300, // Color of the background of the progress bar.
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreen
-                  .shade900), // Color of the progress in the progress bar
-              minHeight: 20),
-          Text(
-            '${(getResource("price")).round()} Euros', // Convert the progress to percentage and round it
-            style: TextStyle(fontSize: 18.0),
-          ),
         ],
       )
     ]
@@ -278,16 +204,22 @@ class MySwitchState extends State<MySwitch> {
       ),
 
       Stack(
-        alignment: Alignment.center,
+        alignment: Alignment.topLeft,
+
         children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child:
+
           Image.asset(
             "assets/home.png",
-            fit: BoxFit.scaleDown
-            ,  // Use BoxFit.fill if you want the image to fill the entire space
+            fit: BoxFit.scaleDown,  // Use BoxFit.fill if you want the image to fill the entire space
+          ),
           ),
           Positioned(
-            top: 640,
-            left: 510,
+            top: 640*scale,
+            left: 510*scale,
+
             child: AnimatedOpacity(
               opacity: _thermostatValue ? 1.0 : 0.0,
               duration: Duration(seconds: 1),
@@ -295,8 +227,8 @@ class MySwitchState extends State<MySwitch> {
             ),
           ),
           Positioned(
-            top: 625,
-            left: 245,
+            top: 625*scale,
+            left: 245*scale,
             child: AnimatedOpacity(
               opacity: _inneKameraValue ? 1.0 : 0.0,
               duration: Duration(seconds: 1),
@@ -304,8 +236,8 @@ class MySwitchState extends State<MySwitch> {
             ),
           ),
           Positioned(
-            top: 480,
-            left: 92,
+            top: 480*scale,
+            left: 92*scale,
             child: AnimatedOpacity(
               opacity: _aussenKameraValue ? 1.0 : 0.0,
               duration: Duration(seconds: 1),
@@ -313,8 +245,8 @@ class MySwitchState extends State<MySwitch> {
             ),
           ),
           Positioned(
-            top: 498,
-            left: 168,
+            top: 498*scale,
+            left: 168*scale,
             child: AnimatedOpacity(
               opacity: _fensterkontaktValue ? 1.0 : 0.0,
               duration: Duration(seconds: 1),
@@ -322,8 +254,9 @@ class MySwitchState extends State<MySwitch> {
             ),
           ),
           Positioned(
-            top: 410,
-            left: 615,
+            top: 410*scale,
+
+            left: 615*scale,
             child: AnimatedOpacity(
               opacity: _lichtsteuerungValue ? 1.0 : 0.0,
               duration: Duration(seconds: 1),
@@ -331,8 +264,8 @@ class MySwitchState extends State<MySwitch> {
             ),
           ),
           Positioned(
-            top: 603,
-            left: 357,
+            top: 603*scale,
+            left: 357*scale,
             child: AnimatedOpacity(
               opacity: _schlossValue ? 1.0 : 0.0,
               duration: Duration(seconds: 1),
@@ -340,11 +273,125 @@ class MySwitchState extends State<MySwitch> {
             ),
           ),
         ],
+      ),Expanded(child:
+    Container(
+
+    height: 1085,
+    margin: EdgeInsets.all(25.0),
+    child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[
+
+
+            Text(
+        'Energy Consumption per Day',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child:
+        LinearProgressIndicator(
+            value: (getResource("energyConsumption") /
+                getFullResource(
+                    "energyConsumption")), // Assuming the price is a percentage of 100
+            backgroundColor: Colors.cyan
+                .shade300, // Color of the background of the progress bar.
+            valueColor: AlwaysStoppedAnimation<Color>(Colors
+                .cyan.shade900), // Color of the progress in the progress bar
+            minHeight: 20),),
+        Text(
+          '${getResource("energyConsumption").round()} kwH', // Convert the progress to percentage and round it
+          style: TextStyle(fontSize: 18.0),
+        ),
+        SizedBox(height: 40),
+        Text(
+          'Carbon Footprint over Product-Lifespan',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child:
+        LinearProgressIndicator(
+            value: (getResource("carbonFootprint") /
+                getFullResource(
+                    "carbonFootprint")), // Assuming the price is a percentage of 100
+            backgroundColor: Colors.brown
+                .shade200, // Color of the background of the progress bar.
+            valueColor: AlwaysStoppedAnimation<Color>(
+                Colors.brown), // Color of the progress in the progress bar
+            minHeight: 20),),
+        Text(
+          '${getResource("carbonFootprint").round()} tons of CO2', // Convert the progress to percentage and round it
+          style: TextStyle(fontSize: 18.0),
+        ),
+        SizedBox(height: 40),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child:CircularProgressIndicator(
+                      value: getResource("security") /
+                          getFullResource("security"),
+                      backgroundColor: Colors.lightBlueAccent.shade100,
+                      color: Colors.blue,
+                      strokeWidth: 10,
+                    )),),
+                SizedBox(height: 10),
+                Text('Security'),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child:CircularProgressIndicator(
+                      value: getResource("comfort") / getFullResource("comfort"),
+                      backgroundColor: Colors.lightBlueAccent.shade100,
+                      color: Colors.blue,
+                      strokeWidth: 10,
+                    )),),
+                SizedBox(height: 20),
+                Text('Comfort'),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(height: 40),
+        Text(
+          'Price',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child:
+        LinearProgressIndicator(
+            value: (getResource("price") /
+                getFullResource(
+                    "price")), // Assuming the price is a percentage of 100
+            backgroundColor: Colors.lightGreen
+                .shade300, // Color of the background of the progress bar.
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreen
+                .shade900), // Color of the progress in the progress bar
+            minHeight: 20),),
+        Text(
+          '${(getResource("price")).round()} Euros', // Convert the progress to percentage and round it
+          style: TextStyle(fontSize: 18.0),
+        ),]
       )
-    ]
+    )),]
     );
   }
-}
+  );
+  }}
+
 
 class SmartHomeGadget {
   String name;
