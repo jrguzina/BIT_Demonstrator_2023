0247de18041afb48b9534af756bc758dd06cab59
Niklas Mathauer <ni352mat@htwg-konstanz.de>
2024-03-21 15:19:37 +0100

    Transportpage erwqeitert

--- lib/Pages/TransportationPage.dart
+++ lib/Pages/TransportationPage.dart
@@ -3,8 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bit_dem_1/Elements/Tile.dart';
 import 'package:countries_world_map/countries_world_map.dart';
-
-enum Selection { ProduktA, ProduktB, ProduktC }
+import 'package:syncfusion_flutter_maps/maps.dart';
 
 class TransportationPage extends StatefulWidget {
   @override
@@ -12,212 +11,248 @@ class TransportationPage extends StatefulWidget {
 }
 
 // Corrected: This should be a State class, not a StatelessWidget.
-class _TransportationPageState extends State<TransportationPage>  {
+class _TransportationPageState extends State<TransportationPage> {
+  bool ressources = false;
+  bool rd = false;
+  bool production = false;
+  bool sales = false;
 
+  bool stateA = false;
+  bool stateB = false;
+  bool stateC = false;
 
+  Color _mapColorToUse = Colors.grey;
 
-  final legendItems = [
-    LegendItem(color: Colors.redAccent, text: "R&D"),
-    LegendItem(color: Colors.yellowAccent, text: "Ressources"),
-    LegendItem(color: Colors.greenAccent, text: "Assembly"),
-    LegendItem(color: Colors.blueAccent, text: "Distribution"),
-    // Add more items as needed
-  ];
-  bool stateA = false; // Example condition for state A
-  bool stateB = false; // Example condition for state B
-  bool stateC = false; // Example condition for state C
-  SMapWorldColors buildMapColors() {
-    // Initialize color variables with null (or a default color if you prefer)
-    Color? colorUS;
-    Color? colorDE;
-    Color? colorCA;
-    Color? colorJP;
-    Color? colorCN;
-    Color? colorCL;
-    Color? colorBR;
-    Color? colorZA;
-    Color? colorCD;
-    Color? colorAO;
-    Color? colorAU;
-    Color? colorTZ;
-    Color? colorET;
-    Color? colorAR;
-    Color? colorCH;
-    Color? colorAT;
-    Color? colorES;
-    Color? colorFR;
-
-    // Use if statements to conditionally assign colors
-    if (stateA) {
-      colorUS = Colors.redAccent;
-      colorDE = Colors.blueAccent;
-      colorBR = Colors.yellowAccent;
-      colorZA = Colors.yellowAccent;
-      colorCL = Colors.yellowAccent;
-      colorES = Colors.greenAccent;
-    }
-    if (stateB) {
-      colorDE = Colors.redAccent;
-      colorCH = Colors.blueAccent;
-      colorAR = Colors.yellowAccent;
-      colorZA = Colors.yellowAccent;
-      colorCL = Colors.yellowAccent;
-      colorCN = Colors.greenAccent;
-    }
-    if (stateC) {
-      colorJP = Colors.redAccent;
-      colorAT = Colors.blueAccent;
-      colorAU = Colors.yellowAccent;
-      colorZA = Colors.yellowAccent;
-      colorCL = Colors.yellowAccent;
-      colorFR = Colors.greenAccent;
-    }
-
-    return SMapWorldColors(
-      uS: colorUS,
-      dE: colorDE,
-      bR: colorBR,
-      zA: colorZA,
-      cL: colorCL,
-      eS: colorES,
-      cH: colorCH,
-      aR: colorAR,
-      cN: colorCN,
-      jP: colorJP,
-      aT: colorAT,
-      aU: colorAU,
-      fR: colorFR,
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Transportation'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              'Auswirkungen auf Transport und Mobilität',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20.0),
-            Tile(
-              title: 'Information',
-              content:
-              'In dieser Grafik können Sie zunächst ein Smart-Home Produkt Ihrer Wahl aussuchen. '
-                  'Basierend darauf zeigt die Karte die Herkunft der unterschiedlichen Komponenten, sowie deren Transportwege und Verarbeitungsschritte in der Lieferkette.',
-            ),
-            SizedBox(height: 20.0),
-            Row(children:<Widget>[
-              Flexible(
-                flex:1,
-                child:
-              Column( mainAxisAlignment: MainAxisAlignment.center,
-                children: [Image.asset('assets/thermostat.png',height: 80,),
-                  SwitchListTile(
-                    title: Text('Thermostat'),
-                    value: stateA,
-                    onChanged: (bool value) {
-                      setState(() {
-                        stateA = value;
-                        stateB = false;
-                        stateC = false;
-                      }
-                      );
-                    },
-                  ),Image.asset('assets/indoor_camera_s.png',height: 80,),
-                  SwitchListTile(
-                    title: Text('Thermostat'),
-                    value: stateB,
-                    onChanged: (bool value) {
-                      setState(() {
-                        stateB = value;
-                        stateA = false;
-                        stateC = false;
-                      }
-                      );
-                    },
-                  ),
-                  Image.asset('assets/outdoor_camera.png',height: 80,),
-                  SwitchListTile(
-                    title: Text('Thermostat'),
-                    value: stateC,
-                    onChanged: (bool value) {
-                      setState(() {
-                        stateC = value;
-                        stateA = false;
-                        stateB = false;
-                      }
-                      );
-                    },
-                  ),],
-              ),
-              ),
-              Flexible(
-                flex:5,
-                child: InteractiveViewer(
-                  minScale: 0.8,
-                  maxScale: 10,
-                 boundaryMargin: const EdgeInsets.all(20),
-                  child:
-                  SimpleMap(
-                    instructions: SMapWorld.instructions,
-                    defaultColor: Colors.grey,
-                    colors: buildMapColors().toMap(),
-                    callback: (id, name, tapdetails) {
-                      print(id);
-                  },
+        appBar: AppBar(
+          title: Text('Transportation'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'Auswirkungen auf Transport und Mobilität',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                 ),
-              ),),
-              Flexible(
-                flex: 1,
-                child: LegendWidget(
-                    items: legendItems),
-              )
-            ]
-            )
-          ]
-        ),
-      ),
-    );
-  }
-  }
+                SizedBox(height: 20.0),
+                Tile(
+                  title: 'Information',
+                  content:
+                      'In dieser Grafik können Sie zunächst ein Smart-Home Produkt Ihrer Wahl aussuchen. '
+                      'Basierend darauf zeigt die Karte die Herkunft der unterschiedlichen Komponenten, sowie deren Transportwege und Verarbeitungsschritte in der Lieferkette.',
+                ),
+                SizedBox(height: 20.0),
+                Flexible(
+                  flex: 5,
+                  child: InteractiveViewer(
+                    minScale: 0.8,
+                    maxScale: 10,
+                    boundaryMargin: const EdgeInsets.all(20),
+                    child: SfMaps(
+                      layers: [
+                        MapShapeLayer(
+                          loadingBuilder: (BuildContext context) {
+                            return const SizedBox(
+                              height: 25,
+                              width: 25,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 3,
+                              ),
+                            );
+                          },
+                          source: const MapShapeSource.asset(
+                            "assets/world_map.json", // Your map shape data source
+                            shapeDataField: "name",
+                          ),
+                          strokeColor: Colors.grey,
+                          strokeWidth:
+                              0.5, // Customization for the map border lines
+                          shapeTooltipBuilder:
+                              (BuildContext context, int index) {
+                            return const Padding(
+                              padding: EdgeInsets.all(2.0),
+                              child: Text('Custom Tooltip'),
+                            );
+                          },
+                          tooltipSettings:
+                              MapTooltipSettings(color: _mapColorToUse),
+                          sublayers: const [
+                            MapArcLayer(
+                              arcs: <MapArc>{
+                                MapArc(
+                                  from: MapLatLng(51.1657,
+                                      10.4515), // Latitude and longitude of Germany
+                                  to: MapLatLng(35.8617,
+                                      104.1954), // Latitude and longitude of China
+                                  color: Colors.blue,
+                                  width: 2,
+                                ),
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.lightbulb),
+                            color: Colors.red[300],
+                            onPressed: () {
+                              setState(() {
+                                ressources = true;
+                                sales = false;
+                                _mapColorToUse = Colors.red[300]!;
+                              });
+                            },
+                          ),
+                          SizedBox(width: 20),
+                          Text("Ressources")
+                        ]),
+                    SizedBox(width: 20),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.lightbulb),
+                            color: Colors.green[300],
+                            onPressed: () {
+                              setState(() {
+                                ressources = true;
+                                sales = false;
+                                _mapColorToUse = Colors.green[300]!;
+                              });
+                            },
+                          ),
+                          Text("R&D")
+                        ]),
+                    SizedBox(width: 20),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.production_quantity_limits),
+                            color: Colors.brown[300],
+                            onPressed: () {
+                              setState(() {
+                                ressources = true;
+                                sales = false;
+                                _mapColorToUse = Colors.brown[300]!;
+                              });
+                            },
+                          ),
+                          Text("Ressources")
+                        ]),
+                    SizedBox(width: 20),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.money),
+                            color: Colors.blue[300],
+                            onPressed: () {
+                              setState(() {
+                                ressources = false;
+                                sales = true;
+                                _mapColorToUse = Colors.blue[300]!;
+                              });
+                            },
+                          ),
+                          Text("Sales")
+                        ]),
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Flexible(
+                  flex: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
 
-class LegendItem {
-  final Color color;
-  final String text;
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(
+                            'assets/thermostat.png',
+                            height: 80, width: 80
+                          ),
+                          Container(width: 250, child:
+                          SwitchListTile(
+                            title: Text('Thermostat'),
+                            value: stateA,
+                            onChanged: (bool value) {
+                              setState(() {
+                                stateA = value;
+                                stateB = false;
+                                stateC = false;
+                              });
+                            },
+                          ),
+                          ),],
+                      ),
 
-  LegendItem({required this.color, required this.text});
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image.asset(
+                              'assets/indoor_camera_s.png',
+                              height: 80, width: 80
+                            ),Container(width:250, child:
+                            SwitchListTile(
+                              title: Text('Indoor Kamera'),
+                              value: stateB,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  stateB = value;
+                                  stateA = false;
+                                  stateC = false;
+                                });
+                              },
+                            ),
+                            ), ]),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset('assets/outdoor_camera.png', height: 80, width: 80,),
+                         Container(width:250,child: SwitchListTile(
+                            title: Text('Outdoor Kamera'),
+                            value: stateC,
+                            onChanged: (bool value) {
+                              setState(() {
+                                stateC = value;
+                                stateA = false;
+                                stateB = false;
+                              });
+                            },
+                          ),
+                         ),],
+                      ),
+
+                    ],
+                  ),
+                ),
+                ],),),
+            );
+  }
 }
-class LegendWidget extends StatelessWidget {
-  final List<LegendItem> items;
-
-  const LegendWidget({Key? key, required this.items}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: items.map((item) => Padding(
-        padding: const EdgeInsets.symmetric(vertical: 2.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: 20,
-              height: 20,
-              color: item.color,
-            ),
-            SizedBox(width: 16),
-            Text(item.text),
-          ],
-        ),
-      )).toList(),
-    );
-  }
-}
-
